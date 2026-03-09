124a7cecac5034d7bc839f28f19996413e33cbea
krishna parihar <krishnaparihar7796@gmail.com>
2026-03-09 23:36:23 +0530

    Update uploader.py

--- uploader.py
+++ uploader.py
@@ -1,11 +1,14 @@
 
 async def upload_file(client, path, channel):
 
-    if path.endswith(".mp4"):
+    if path.endswith(".mp4") or path.endswith(".mkv"):
+
         await client.send_video(channel, path)
 
     elif path.endswith(".jpg") or path.endswith(".png"):
+
         await client.send_photo(channel, path)
 
     else:
+
         await client.send_document(channel, path)
