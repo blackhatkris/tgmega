53188fabed396da93f474bec15fabca2184d2247
krishna parihar <krishnaparihar7796@gmail.com>
2026-03-09 22:06:19 +0530

    Update mega_downloader.py

--- mega_downloader.py
+++ mega_downloader.py
@@ -2,48 +2,26 @@
 import os
 from mega import Mega
 from uploader import upload_file
-from config import MAX_FILE_SIZE_MB, DOWNLOAD_DIR
 
 mega = Mega()
 
-async def process_mega_link(client, link, target_channel, progress_msg):
-
-    os.makedirs(DOWNLOAD_DIR, exist_ok=True)
+async def start_mega_task(client, link, channel):
 
     m = mega.login()
 
     files = m.get_files()
 
-    uploaded = 0
-    skipped = 0
+    for f in files:
 
-    for file in files:
+        info = files[f]
 
-        info = files[file]
-
-        if info['t'] != 0:
+        if info["t"] != 0:
             continue
 
-        size = int(info['s']) / (1024 * 1024)
+        name = info["a"]["n"]
 
-        if size > MAX_FILE_SIZE_MB:
-            skipped += 1
-            continue
+        path = m.download(f)
 
-        name = info['a']['n']
-
-        await progress_msg.edit_text(f"Downloading {name}")
-
-        path = m.download(file, DOWNLOAD_DIR)
-
-        await progress_msg.edit_text(f"Uploading {name}")
-
-        await upload_file(client, path, target_channel)
+        await upload_file(client, path, channel)
 
         os.remove(path)
-
-        uploaded += 1
-
-        await progress_msg.edit_text(
-            f"Uploaded: {uploaded}\nSkipped: {skipped}"
-        )
