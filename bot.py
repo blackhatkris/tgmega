35f2a05e5f861f1f23596407594a1651b6b15ab2
krishna parihar <krishnaparihar7796@gmail.com>
2026-03-09 22:05:33 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,30 +1,70 @@
 
+import os
 from pyrogram import Client, filters
-from mega_downloader import process_mega_link
-from config import API_ID, API_HASH, BOT_TOKEN, TARGET_CHANNEL
+from pyrogram.types import Message
+from mega_downloader import start_mega_task
 
-app = Client(
-    "mega-bot",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    bot_token=BOT_TOKEN
-)
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+
+app = Client("mega-bot", bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_HASH)
+
+user_channel = {}
+pending_link = {}
 
 @app.on_message(filters.command("start"))
-async def start(client, message):
-    await message.reply_text("Bot ready. Send /mega <link>")
+async def start(client, message: Message):
+    await message.reply("Send /setchannel <channel_id>")
+
+@app.on_message(filters.command("setchannel"))
+async def set_channel(client, message: Message):
+
+    channel_id = int(message.command[1])
+
+    try:
+        member = await client.get_chat_member(channel_id, "me")
+    except:
+        await message.reply("Bot is not in that channel.")
+        return
+
+    if member.status not in ["administrator", "creator"]:
+        await message.reply("Bot must be admin.")
+        return
+
+    user_channel[message.from_user.id] = channel_id
+
+    await message.reply("Channel saved. Now send /mega")
 
 @app.on_message(filters.command("mega"))
 async def mega(client, message):
 
-    if len(message.command) < 2:
-        await message.reply_text("Send /mega <mega_link>")
+    if message.from_user.id not in user_channel:
+        await message.reply("Set channel first.")
         return
 
-    link = message.command[1]
+    await message.reply("Send MEGA link.")
 
-    msg = await message.reply_text("Scanning MEGA link...")
+@app.on_message(filters.text & ~filters.command)
+async def receive_link(client, message):
 
-    await process_mega_link(client, link, TARGET_CHANNEL, msg)
+    if "mega.nz" in message.text:
 
-app.run()
+        pending_link[message.from_user.id] = message.text
+
+        await message.reply("Start process? Reply YES")
+
+@app.on_message(filters.text & filters.regex("YES"))
+async def start_process(client, message):
+
+    user = message.from_user.id
+
+    if user not in pending_link:
+        return
+
+    link = pending_link[user]
+    channel = user_channel[user]
+
+    await message.reply("Starting task...")
+
+    await start_mega_task(client, link, channel)
