dbc5eedd0941453c14c42461bade5e190ebf4e6b
krishna parihar <krishnaparihar7796@gmail.com>
2026-03-10 15:51:39 +0530

    Update uploader.py

--- uploader.py
+++ uploader.py
@@ -3,29 +3,20 @@ import os
 
 async def upload_file(client, path, channel):
 
-    file_name = os.path.basename(path).lower()
+    name = os.path.basename(path).lower()
 
-    # VIDEO FILES
-    if file_name.endswith((".mp4", ".mkv", ".mov", ".webm")):
+    if name.endswith((".mp4",".mkv",".mov",".webm")):
 
         await client.send_video(
-            chat_id=channel,
-            video=path,
+            channel,
+            path,
             supports_streaming=True
         )
 
-    # IMAGE FILES
-    elif file_name.endswith((".jpg", ".jpeg", ".png")):
+    elif name.endswith((".jpg",".jpeg",".png")):
 
-        await client.send_photo(
-            chat_id=channel,
-            photo=path
-        )
+        await client.send_photo(channel, path)
 
-    # OTHER FILES
     else:
 
-        await client.send_document(
-            chat_id=channel,
-            document=path
-        )
+        await client.send_document(channel, path)
